e5b1550c6283f565ba0edfcb959c299eee7bce1f
Silvan Fuhrer <silvan@auterion.com>
2024-09-05 11:20:39 +0200

    Commander: add VEHICLE_CMD_EXTERNAL_POSITION_ESTIMATE to list of externaly handled commands (#23642)
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -1513,7 +1513,6 @@ Commander::handle_command(const vehicle_command_s &cmd)
 	case vehicle_command_s::VEHICLE_CMD_DO_WINCH:
 	case vehicle_command_s::VEHICLE_CMD_DO_GRIPPER:
 	case vehicle_command_s::VEHICLE_CMD_EXTERNAL_POSITION_ESTIMATE:
-	case vehicle_command_s::VEHICLE_CMD_REQUEST_CAMERA_INFORMATION:
 		/* ignore commands that are handled by other parts of the system */
 		break;
 
