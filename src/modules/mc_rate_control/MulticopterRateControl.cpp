9cebc7faf5f9edfc09144b1006186817b48bc68a
Ali Barari <Barari@avestec.com>
2024-09-24 13:47:27 -0700

    Acro update

--- src/modules/mc_rate_control/MulticopterRateControl.cpp
+++ src/modules/mc_rate_control/MulticopterRateControl.cpp
@@ -100,21 +100,29 @@ MulticopterRateControl::parameters_updated()
 float
 MulticopterRateControl::throttle_curve(float throttle_stick_input)
 {
-	float thrust = 0.f;
+	// float thrust = 0.f;
 
+	// switch (_param_mpc_thr_curve.get()) {
+	// case 1: // no rescaling to hover throttle
+	// 	thrust = math::interpolate(throttle_stick_input, -1.f, 1.f,
+	// 				   _manual_throttle_minimum.getState(), _param_mpc_thr_max.get());
+	// 	break;
+
+	// default: // 0 or other: rescale such that a centered throttle stick corresponds to hover throttle
+	// 	thrust = math::interpolateNXY(throttle_stick_input, {-1.f, 0.f, 1.f},
+	// 	{_manual_throttle_minimum.getState(), _param_mpc_thr_hover.get(), _param_mpc_thr_max.get()});
+	// 	break;
+	// }
+
+	// return math::min(thrust, _manual_throttle_maximum.getState());
+	// throttle_stick_input is in range [0, 1]
 	switch (_param_mpc_thr_curve.get()) {
 	case 1: // no rescaling to hover throttle
-		thrust = math::interpolate(throttle_stick_input, -1.f, 1.f,
-					   _manual_throttle_minimum.getState(), _param_mpc_thr_max.get());
-		break;
+		return math::interpolate(throttle_stick_input, 0.f, 1.f, _param_mpc_manthr_min.get(), _param_mpc_thr_max.get());
 
-	default: // 0 or other: rescale such that a centered throttle stick corresponds to hover throttle
-		thrust = math::interpolateNXY(throttle_stick_input, {-1.f, 0.f, 1.f},
-		{_manual_throttle_minimum.getState(), _param_mpc_thr_hover.get(), _param_mpc_thr_max.get()});
-		break;
+	default: // 0 or other: rescale to hover throttle at 0.5 stick
+		return math::interpolateN(throttle_stick_input, {_param_mpc_manthr_min.get(), _param_mpc_thr_hover.get(), _param_mpc_thr_max.get()});
 	}
-
-	return math::min(thrust, _manual_throttle_maximum.getState());
 }
 
 void
